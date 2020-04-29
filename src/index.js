cb555d259a7dce860482180f01e4523fbfb9f7aa
Colin McNamara <coolinmc6@gmail.com>
2020-04-29 11:57:07 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './css/styles.scss';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Main App</h1>
+    </main>
+  )
+}
+
+render(<App />, document.getElementById('root'));
